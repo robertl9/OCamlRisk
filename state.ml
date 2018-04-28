2b1e1493b207964a94f2d540732fcf044c8d3690
Milan Rakesh Shah <mrs282@cornell.edu>
2018-04-28 13:42:00 -0400

    changes

--- state.ml
+++ state.ml
@@ -16,7 +16,7 @@ type character = JonSnow | DaenerysTargareyan | NightKing | Bran
 
 type card = BannerMan | Lord | Dragon | WildCard
 
-type phase = Reinforce | Deploy | Attack
+type phase = Start | Reinforce | Deploy | Attack
 
 type player = {
   character: character;
@@ -27,15 +27,16 @@ type player = {
 
 type state = {
   players_list: player list;
-  characters_list: character list;
+  (* characters_list: character list; *)
   current_turn: string;               (* string representing id of character's turn *)
   c_phase: phase;
   continents: continent list;
   countries: country list;
-  card_l: card list;
-  num_troops_left: int;
-  phase: phase ;
+  card_l: card array;
+  (* num_troops_left: int; *)
+  (* phase: phase ; *)
   fog_of_war: bool;
+  w_msg: string;
 }
 
 let roll n =
@@ -49,55 +50,70 @@ let roll_m n =
   for i = 0 to n-1 do rl.(i)<-(Random.int 5 + 1) done
 
 let init_characters () =
-  let lst = Bran::NightKing::DaenerysTargareyan::JonSnow::[] in
-  lst
+  [Bran; NightKing; DaenerysTargareyan; JonSnow]
 
-let init_state file =
+let init_cards () =
+  let bm = Array.make 15 BannerMan in
+  let l = Array.make 15 Lord in
+  let d = Array.make 15 Dragon in
+  let wc = Array.make 2 WildCard in
+  let bml = Array.append bm l in
+  let bmld = Array.append bml d in
+  let deck = Array.append bmld wc in
+  deck
 
-  
+let init_state j =
+  let continents = j|> member "continents" |> to_list in
+  let countries = j|> member "countries" |> to_list in
+  let win = j|> member "win_message" |> to_string in
+  {players_list = []; current_turn = ""; c_phase = Start;
+   continents = continents; countries = countries; card_l = init_cards ();
+   fog_of_war = false; w_msg = win;}
+
+
 
 (* [taken s p] returns a list representing the countries in s
  * that are held by a player p
  *)
-val taken_by: state -> player -> string list
+(* val taken_by: state -> player -> string list *)
 
 (* [available s] is a list of countries
  * that are not taken by a player, on teh board
  *)
-val available: state -> string list
+(* val available: state -> string list *)
 
 (* [continents] list of continent names*)
-val continents: state -> string list
+(* val continents: state -> string list *)
 
 (* [countries] list of country names*)
-val countries: state -> string list
+(* val countries: state -> string list *)
 
 (* [exits c] returns a list of the names of countries that neighbor c *)
-val exits: country -> string list
+(* val exits: country -> string list *)
 
 (* [num_troops s p] *)
-val num_troops: state -> int
+(* val num_troops: state -> int *)
 
 (* [turns_by p] is a number representing the number of turns taken by a player.
  * A turn consists of three stages: p deploying their troops, p declaring an attack
  * on a country held by another player, and p reinforcing their troops
  *)
-val turns_by: player -> int
+(* val turns_by: player -> int *)
 
 (* [win s] returns a state in which the player who owns all of the
  * countries in the state wins the game. If no such player exists, the
  * same state is returned.
  *)
-val win: state -> state
+(* val win: state -> state *)
 
 (* [cards_owned p] returns a list of strings representing cards owned by p*)
-val cards_owned: player -> string list
+(* val cards_owned: player -> string list *)
 
 (* [remove_card s c] returns a state that has a
  * a card list that does not contain c
  *)
-val remove_card: state -> card -> state
+(* val remove_card: state -> card -> state *)
 
 (* [cards_free s] returns a list of cards not held by any player
  *)
-val cards_free: state -> string list
+(* val cards_free: state -> string list *)
