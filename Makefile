909ffce01a78ba812d8a41afd1aabaa46e6efd3a
Abdullah Islam <ai.islam1433@gmail.com>
2018-05-01 16:55:26 -0400

    Update to gui and Makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 gui:
 
-	rm -f gui && rm -f gui.cmo && rm -f gui.cmi && ocamlc -I +../lablgtk2 -o gui 		lablgtk.cma gtkInit.cmo gui.ml && ./gui
+	rm -f gui && rm -f gui.cmo && rm -f gui.cmi && ocamlbuild -clean && ocamlc -I +../lablgtk2 -o gui lablgtk.cma gtkInit.cmo gui.ml && ./gui
 
 clean: 
 	rm -f gui && rm -f gui.cmo && rm -f gui.cmi && ocamlbuild -clean
--- gui.ml
+++ gui.ml
@@ -1,55 +1,29 @@
-
-
-
+let rec selectPlayers num vbox=
+  let p6 = GButton.button ~label:"6 Players" ~packing:vbox#add () in
+  let _ =p6#connect#clicked ~callback: (fun () -> selectPlayers 6 vbox) in
+  prerr_endline (string_of_int num)
 
 let main () =
-let window = GWindow.window ~border_width:20 ~title:"Risk" () in
-let box1 = GPack.vbox ~border_width: 20 ~packing:window#add () in
-let box2 = GPack.hbox ~border_width: 20 ~packing:box1#add () in
-let box3 = GPack.hbox ~border_width: 20 ~packing:box1#add () in
-let twoPlayer = GButton.button ~label:"2 Players" ~packing:box2#pack () in
-let threePlayer = GButton.button ~label:"3 Players" ~packing:box2#pack () in
-let fourPlayer = GButton.button ~label:"4 Players" ~packing:box3#pack () in
-let fivePlayer = GButton.button ~label:"5 Players" ~packing:box3#pack () in
-
+  let window = GWindow.window ~title:"Risk" ~border_width:10 () in
+  let _ = window #connect#destroy ~callback:GMain.Main.quit in
+  let main_vbox = GPack.vbox ~packing:window#add () in
+  let title = GMisc.label ~text:"Risk" ~packing:main_vbox#add ~height:50 () in
+  let instruct = GMisc.label ~text:"Please Select the Number of Players" ~packing:main_vbox#add () in
+  let vbox = GPack.vbox ~border_width:10 ~packing:main_vbox#add () in
+  let hbox1 = GPack.button_box `HORIZONTAL ~border_width:20 ~child_width:100 ~child_height:100
+      ~spacing:20 ~packing:vbox#add () in
+  let hbox2 = GPack.button_box `HORIZONTAL ~border_width:20 ~child_width:100 ~child_height:100
+      ~spacing:20 ~packing:vbox#add () in
+  let p2 = GButton.button ~label:"2 Players" ~packing:hbox1#add () in
+  let _ =p2#connect#clicked ~callback: (fun () -> selectPlayers 2 vbox) in
+  let p3 = GButton.button ~label:"3 Players" ~packing:hbox1#add () in
+  let _ = p3#connect#clicked ~callback: (fun () -> selectPlayers 3 vbox) in
+  let p4 = GButton.button ~label:"4 Players" ~packing:hbox2#add () in
+  let _ = p4#connect#clicked ~callback: (fun () -> selectPlayers 4 vbox) in
+  let p5 = GButton.button ~label:"5 Players" ~packing:hbox2#add () in
+  let _ =p5#connect#clicked ~callback: (fun () -> selectPlayers 5 vbox) in
   window#show ();
+  (* Enter the event loop *)
   GMain.Main.main ()
 
-let _ = main ()
-
-
-
-
-(* open GMain
-open GdkKeysyms
-
-let locale = GtkMain.Main.init ()
-
-let main () =
-  let window = GWindow.window ~width:800 ~height:800
-                              ~title:"Risk" () in
-  let vbox = GPack.vbox ~packing:window#add () in
-  window#connect#destroy ~callback:Main.quit;
-
-  (* Menu bar *)
-  let menubar = GMenu.menu_bar ~packing:vbox#pack () in
-  let factory = new GMenu.factory menubar in
-  let accel_group = factory#accel_group in
-  let file_menu = factory#add_submenu "File" in
-
-  (* File menu *)
-  let factory = new GMenu.factory file_menu ~accel_group in
-  factory#add_item "Quit" ~key:_Q ~callback: Main.quit;
-
-  (* Button *)
-  let button = GButton.button ~label:"Push me!"
-                              ~packing:vbox#add () in
-  button#connect#clicked ~callback: (fun () -> prerr_endline "Ouch!");
-
-  (* Display the windows and enter Gtk+ main loop *)
-  window#add_accel_group accel_group;
-  window#show ();
-  Main.main ()
-
-
-let () = main () *)
+let _ =  main ()
