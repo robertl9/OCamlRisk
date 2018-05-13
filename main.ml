0d2e9ba2d6f9ce642f5e19d0c05241586c2b25a0
Milan Rakesh Shah <mrs282@cornell.edu>
2018-05-13 15:16:11 -0400

    81 they bring the crashers to the party

--- main.ml
+++ main.ml
@@ -23,10 +23,10 @@ let rec updateTerm state =
     |_->updateTerm updatedState
 
 (* [play_game f] plays the game in adventure file [f]. *)
-let play_game p ai =
+let play_game p eAI mAI hAI =
   let w = GWindow.window ~title:"Risk" ~border_width:10 () in
   let _ = w#connect#destroy ~callback:GMain.Main.quit in
-  let startState = (init_state (int_of_string p) (int_of_string ai) (Yojson.Basic.from_file "map.json")) in
+  let startState = (init_state p eAI mAI hAI (Yojson.Basic.from_file "map.json")) in
   draw w [] startState
   (* updateTerm startState *)
 
@@ -35,8 +35,10 @@ let play_game p ai =
  * still prompt for a game to play rather than hardcode a game file. *)
 let main () =
   let s = init_gui () in
-  let p = String.make 1 (String.get s 0) in
-  let ai = String.make 1 (String.get s 2) in
-  play_game p ai
+  let p = (int_of_string (String.make 1 (String.get s 0))) in
+  let eAI = (int_of_string (String.make 1 (String.get s 2))) in
+  let mAI = (int_of_string (String.make 1 (String.get s 4))) in
+  let hAI = (int_of_string (String.make 1 (String.get s 6))) in
+  play_game p eAI mAI hAI
 
 let () = main ()
