3d5221192239453b15696f2d590e8598519abc03
Robert Li <robertli@dhcp-mcfad-1958.redrover.cornell.edu>
2018-05-14 23:19:40 -0400

    reinforce fixed

--- gui.ml
+++ gui.ml
@@ -9,7 +9,7 @@ let _ = GtkMain.Main.init ()
 let winningScreen box msg w =
   let _ = box#destroy () in
   let main_hbox = GPack.hbox ~packing:w#add ~width:1200 ~height:700 () in
-  let big_label = GMisc.label ~text:msg ~packing:main_hbox#add () in
+  let _ = GMisc.label ~text:msg ~packing:main_hbox#add () in
   GMain.Main.quit ()
 
 let rec action cl box w st =
@@ -32,10 +32,10 @@ let rec action cl box w st =
           else let st' = do' (AttackC (List.hd (List.rev cl), List.hd cl)) st in draw w [] st'
         | Reinforce ->
         if List.length cl = 1 then draw w cl st
-        else let plyr = get_cplayer st in
-          (* let fst_ctry = get_country_id (List.hd(List.rev cl)) in
-          let num_troops = get_troops fst_ctry plyr in *)
-          let st' = do' (ReinforceC (1, List.hd (List.rev cl), List.hd cl)) st in draw w [] st'
+        else let plyr = get_player_of_state st in
+          let fst_ctry = (List.hd(List.rev cl)) in
+          let num_troops = get_troops fst_ctry plyr in
+          let st' = do' (ReinforceC (num_troops - 1, List.hd (List.rev cl), List.hd cl)) st in draw w [] st'
 and aiAction box w st =
   let _ = print_string "ai actioning\n" in
   let _ = box#destroy () in
@@ -52,8 +52,8 @@ and draw window cl st =
   let _ = has_won st in
   let gameBoard = GPack.fixed ~packing:main_hbox#add () in
   let _ = GMisc.image ~file: "images/riskmap.png" ~packing:(gameBoard#put ~x:0 ~y:0) () in
-  let currentPlayer = GMisc.label ~text:(get_cplayer st) ~packing:(gameBoard#put ~x:1090 ~y:15) () in
-  let consoleMessage = GMisc.label ~text:((get_msg st)^"\n"^(getPhaseString st)^"\n"^(printOrder st)) ~packing:(gameBoard#put ~x:1025 ~y:525) () in
+  let _ = GMisc.label ~text:(get_cplayer st) ~packing:(gameBoard#put ~x:1090 ~y:15) () in
+  let _ = GMisc.label ~text:((get_msg st)^"\n"^(getPhaseString st)^"\n"^(printOrder st)) ~packing:(gameBoard#put ~x:1025 ~y:525) () in
   let right_buttons = GPack.button_box `VERTICAL ~border_width:0 ~child_width:160 ~child_height:30
       ~spacing:10 ~packing:(gameBoard#put ~x:1020 ~y:40) () in
 
@@ -126,7 +126,7 @@ and draw window cl st =
   let countryA = GButton.button ~label:"Winter" ~packing:(gameBoard#put ~x:120 ~y:30) () in
   countryA#misc#set_name (button_color_determiner st countryA#label);
   (* let _ = countryA# `WHITE *)
-  let countryALabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryA#label) countryTroops)) with
       | _ -> "0")
       ~packing:(gameBoard#put ~x:138 ~y:55) () in
@@ -134,91 +134,91 @@ and draw window cl st =
   let countryB = GButton.button ~label:"Winterfell" ~packing:(gameBoard#put ~x:140 ~y:120) () in
   countryB#misc#set_name (button_color_determiner st countryB#label);
   let _ = countryB#connect#clicked ~callback: (fun () -> action (countryB#label::cl) main_hbox window st) in
-  let countryBLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryB#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x:165 ~y:145) () in
   let countryC = GButton.button ~label:"Riverrun" ~packing:(gameBoard#put ~x:180 ~y:180) () in
   countryC#misc#set_name (button_color_determiner st countryC#label);
   let _ = countryC#connect#clicked ~callback: (fun () -> action (countryC#label::cl) main_hbox window st) in
-  let countryCLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryC#label) countryTroops)) with
       | _ -> "0")
       ~packing:(gameBoard#put ~x:200 ~y:205) () in
   let countryD = GButton.button ~label:"Vale" ~packing:(gameBoard#put ~x:178 ~y:240) () in
   countryD#misc#set_name (button_color_determiner st countryD#label);
   let _ = countryD#connect#clicked ~callback: (fun () -> action (countryD#label::cl) main_hbox window st) in
-  let countryDLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryD#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x:192 ~y:265) () in
   let countryE = GButton.button ~label:"Casterly\nRock" ~packing:(gameBoard#put ~x:110 ~y:280) () in
   countryE#misc#set_name (button_color_determiner st countryE#label);
   let _ = countryE#connect#clicked ~callback: (fun () -> action (countryE#label::cl) main_hbox window st) in
-  let countryELabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryE#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x:129 ~y:320) () in
   let countryF = GButton.button ~label:"Dragon-\nstone" ~packing:(gameBoard#put ~x:203 ~y:325) () in
   countryF#misc#set_name (button_color_determiner st countryF#label);
   let _ = countryF#connect#clicked ~callback: (fun () -> action (countryF#label::cl) main_hbox window st) in
-  let countryFLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryF#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x:225 ~y:365) () in
   let countryG = GButton.button ~label:"Storm's End" ~packing:(gameBoard#put ~x:120 ~y:360) () in
   countryG#misc#set_name (button_color_determiner st countryG#label);
   let _ = countryG#connect#clicked ~callback: (fun () -> action (countryG#label::cl) main_hbox window st) in
-  let countryGLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryG#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x:150 ~y:385) () in
   let countryH = GButton.button ~label:"Dorne" ~packing:(gameBoard#put ~x:170 ~y:457) () in
   countryH#misc#set_name (button_color_determiner st countryH#label);
   let _ = countryH#connect#clicked ~callback: (fun () -> action (countryH#label::cl) main_hbox window st) in
-  let countryHLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryH#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x:185 ~y:482) () in
   let countryJ = GButton.button ~label:"Pentos" ~packing:(gameBoard#put ~x:455 ~y:320) () in
   countryJ#misc#set_name (button_color_determiner st countryJ#label);
   let _ = countryJ#connect#clicked ~callback: (fun () -> action (countryJ#label::cl) main_hbox window st) in
-  let countryJLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryJ#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 470 ~y:345) () in
   let countryK = GButton.button ~label:"Braavos" ~packing:(gameBoard#put ~x:425 ~y:220) () in
   countryK#misc#set_name (button_color_determiner st countryK#label);
   let _ = countryK#connect#clicked ~callback: (fun () -> action (countryK#label::cl) main_hbox window st) in
-  let countryKLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryK#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 445 ~y:245) () in
   let countryL = GButton.button ~label:"Tyrosh" ~packing:(gameBoard#put ~x:440 ~y:410) () in
   countryL#misc#set_name (button_color_determiner st countryL#label);
   let _ = countryL#connect#clicked ~callback: (fun () -> action (countryL#label::cl) main_hbox window st) in
-  let countryLLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryL#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 457 ~y:435) () in
   let countryM = GButton.button ~label:"Dothraki Sea" ~packing:(gameBoard#put ~x:645 ~y:265) () in
   countryM#misc#set_name (button_color_determiner st countryM#label);
   let _ = countryM#connect#clicked ~callback: (fun () -> action (countryM#label::cl) main_hbox window st) in
-  let countryMLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryM#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 680 ~y:290) () in
   let countryN = GButton.button ~label:"Volantis" ~packing:(gameBoard#put ~x:545 ~y:425) () in
   countryN#misc#set_name (button_color_determiner st countryN#label);
   let _ = countryN#connect#clicked ~callback: (fun () -> action (countryN#label::cl) main_hbox window st) in
-  let countryNLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryN#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 563 ~y:450) () in
   let countryO = GButton.button ~label:"Slaver Bay" ~packing:(gameBoard#put ~x:646 ~y:395) () in
   countryO#misc#set_name (button_color_determiner st countryO#label);
   let _ = countryO#connect#clicked ~callback: (fun () -> action (countryO#label::cl) main_hbox window st) in
-  let countryOLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryO#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 661 ~y:420) () in
@@ -226,42 +226,42 @@ and draw window cl st =
   let countryP = GButton.button ~label:"Unknown Lands" ~packing:(gameBoard#put ~x:800 ~y:200) () in
   countryP#misc#set_name (button_color_determiner st countryP#label);
   let _ = countryP#connect#clicked ~callback: (fun () -> action (countryP#label::cl) main_hbox window st) in
-  let countryPLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryP#label) countryTroops)) with
       | _ -> "0")
       ~packing:(gameBoard#put ~x: 835 ~y:225) () in
   let countryQ = GButton.button ~label:"Red Waste" ~packing:(gameBoard#put ~x:760 ~y:420) () in
   countryQ#misc#set_name (button_color_determiner st countryQ#label);
   let _ = countryQ#connect#clicked ~callback: (fun () -> action (countryQ#label::cl) main_hbox window st) in
-  let countryQLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryQ#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 790 ~y:445) () in
   let countryR = GButton.button ~label:"Skagos" ~packing:(gameBoard#put ~x:280 ~y:100) () in
   countryR#misc#set_name (button_color_determiner st countryR#label);
   let _ = countryR#connect#clicked ~callback: (fun () -> action (countryR#label::cl) main_hbox window st) in
-  let countryRLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryR#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 300 ~y:125) () in
   let countryS = GButton.button ~label:"Iron Islands" ~packing:(gameBoard#put ~x:10 ~y:220) () in
   countryS#misc#set_name (button_color_determiner st countryS#label);
   let _ = countryS#connect#clicked ~callback: (fun () -> action (countryS#label::cl) main_hbox window st) in
-  let countrySLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryS#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 45 ~y:245) () in
   let countryT = GButton.button ~label:"Highgarden" ~packing:(gameBoard#put ~x:10 ~y:440) () in
   countryT#misc#set_name (button_color_determiner st countryT#label);
   let _ = countryT#connect#clicked ~callback: (fun () -> action (countryT#label::cl) main_hbox window st) in
-  let countryTLabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryT#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 37 ~y:470) () in
   let countryU = GButton.button ~label:"Qohor" ~packing:(gameBoard#put ~x:520 ~y:240) () in
   countryU#misc#set_name (button_color_determiner st countryU#label);
   let _ = countryU#connect#clicked ~callback: (fun () -> action (countryU#label::cl) main_hbox window st) in
-  let countryULabel = GMisc.label ~text:
+  let _ = GMisc.label ~text:
       (try (string_of_int (List.assoc (String.uppercase_ascii countryU#label) countryTroops)) with
        | _ -> "0")
       ~packing:(gameBoard#put ~x: 538 ~y:265) () in
@@ -288,35 +288,35 @@ and draw window cl st =
   let aDice =
     match getAttackDice st with
     | [] ->
-      (let a_dice1 = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1020 ~y:300) () in
-       let a_dice2 = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1080 ~y:300) () in
-       let a_dice3 = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1140 ~y:300) () in ())
+      (let _ = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1020 ~y:300) () in
+       let _ = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1080 ~y:300) () in
+       let _ = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1140 ~y:300) () in ())
     | x::[] ->
-      (let a_dice1 = GMisc.image ~file: ("images/dice"^(string_of_int x)^".png") ~packing:(gameBoard#put ~x:1020 ~y:300) () in
-      let a_dice2 = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1080 ~y:300) () in
-       let a_dice3 = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1140 ~y:300) () in ())
+      (let _ = GMisc.image ~file: ("images/dice"^(string_of_int x)^".png") ~packing:(gameBoard#put ~x:1020 ~y:300) () in
+      let _ = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1080 ~y:300) () in
+       let _ = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1140 ~y:300) () in ())
     | x::x'::[] ->
-      (let a_dice1 = GMisc.image ~file: ("images/dice"^(string_of_int x)^".png") ~packing:(gameBoard#put ~x:1020 ~y:300) () in
-      let a_dice2 = GMisc.image ~file: ("images/dice"^(string_of_int x')^".png") ~packing:(gameBoard#put ~x:1080 ~y:300) () in
-       let a_dice3 = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1140 ~y:300) () in ())
+      (let _ = GMisc.image ~file: ("images/dice"^(string_of_int x)^".png") ~packing:(gameBoard#put ~x:1020 ~y:300) () in
+      let _ = GMisc.image ~file: ("images/dice"^(string_of_int x')^".png") ~packing:(gameBoard#put ~x:1080 ~y:300) () in
+       let _ = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1140 ~y:300) () in ())
     | x::x'::x''::[] ->
-      (let a_dice1 = GMisc.image ~file: ("images/dice"^(string_of_int x)^".png") ~packing:(gameBoard#put ~x:1020 ~y:300) () in
-      let a_dice2 = GMisc.image ~file: ("images/dice"^(string_of_int x')^".png") ~packing:(gameBoard#put ~x:1080 ~y:300) () in
-       let a_dice3 = GMisc.image ~file: ("images/dice"^(string_of_int x'')^".png") ~packing:(gameBoard#put ~x:1140 ~y:300) () in ())
+      (let _ = GMisc.image ~file: ("images/dice"^(string_of_int x)^".png") ~packing:(gameBoard#put ~x:1020 ~y:300) () in
+      let _ = GMisc.image ~file: ("images/dice"^(string_of_int x')^".png") ~packing:(gameBoard#put ~x:1080 ~y:300) () in
+       let _ = GMisc.image ~file: ("images/dice"^(string_of_int x'')^".png") ~packing:(gameBoard#put ~x:1140 ~y:300) () in ())
     | _ -> (failwith "Invalid Roll") in
   let _ = aDice in
 
   let dDice =
     match getDefendDice st with
     | [] ->
-      (let d_dice1 = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1040 ~y:400) () in
-       let d_dice2 = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1120 ~y:400) () in ())
+      (let _ = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1040 ~y:400) () in
+       let _ = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1120 ~y:400) () in ())
     | x::[] ->
-      (let d_dice1 = GMisc.image ~file: ("images/dice"^(string_of_int x)^".png") ~packing:(gameBoard#put ~x:1040 ~y:400) () in
-       let d_dice2 = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1120 ~y:400) () in ())
+      (let _ = GMisc.image ~file: ("images/dice"^(string_of_int x)^".png") ~packing:(gameBoard#put ~x:1040 ~y:400) () in
+       let _ = GMisc.image ~file: "images/nodice.png" ~packing:(gameBoard#put ~x:1120 ~y:400) () in ())
     | x::x'::[] ->
-      (let d_dice1 = GMisc.image ~file: ("images/dice"^(string_of_int x)^".png") ~packing:(gameBoard#put ~x:1040 ~y:400) () in
-       let d_dice2 = GMisc.image ~file: ("images/dice"^(string_of_int x')^".png") ~packing:(gameBoard#put ~x:1120 ~y:400) () in ())
+      (let _ = GMisc.image ~file: ("images/dice"^(string_of_int x)^".png") ~packing:(gameBoard#put ~x:1040 ~y:400) () in
+       let _ = GMisc.image ~file: ("images/dice"^(string_of_int x')^".png") ~packing:(gameBoard#put ~x:1120 ~y:400) () in ())
     | _ -> (failwith "Invalid Roll") in
   let _ = dDice in
 
